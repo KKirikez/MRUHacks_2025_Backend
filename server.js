067b792cce39ee7182f8b51e7060d11a82a2dc21
kkirikez <kostyakirikeza@gmail.com>
2025-10-04 18:01:17 -0600

    Serial connection works after debugging

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const express = require('express');
+const { SerialPort } = require('serialport');
+const { ReadlineParser } = require('@serialport/parser-readline');
+
 const app = express();
+
+const SERIAL_PATH = process.env.SERIAL_PATH || '/dev/ttyACM0';
+const BAUD = 115200;
+
+const port = new SerialPort({ path: SERIAL_PATH, baudRate: BAUD });
+const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
+
+port.on('open', () => {
+  console.log('Serial connected on', SERIAL_PATH);
+});
+
+parser.on('data', line => {
+  console.log('Arduino ->', line);
+});
 
 app.get('/ping', (req, res) => {
   res.json({ ok: true, message: 'Server is alive' });
